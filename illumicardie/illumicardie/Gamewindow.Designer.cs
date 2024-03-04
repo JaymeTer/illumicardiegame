216bf1eb58606514d78b199f5f5d6e80d37be9c8
Jayme <159971048+JaymeTer@users.noreply.github.com>
2024-03-04 15:07:03 -0800

    1st

---
+++ illumicardie/illumicardie/Gamewindow.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace illumicardie
+{
+    partial class Gamewindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Gamewindow));
+            this.Playerhand1 = new System.Windows.Forms.PictureBox();
+            this.Playerhand2 = new System.Windows.Forms.PictureBox();
+            this.Deck = new System.Windows.Forms.PictureBox();
+            this.Playerhand3 = new System.Windows.Forms.PictureBox();
+            this.Playerhand4 = new System.Windows.Forms.PictureBox();
+            this.Playerhand5 = new System.Windows.Forms.PictureBox();
+            this.Playerhand6 = new System.Windows.Forms.PictureBox();
+            this.Dealbutton = new System.Windows.Forms.Button();
+            this.Playertotal = new System.Windows.Forms.Label();
+            this.Hitbutton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Deck)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Playerhand1
+            // 
+            this.Playerhand1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Playerhand1.Location = new System.Drawing.Point(146, 533);
+            this.Playerhand1.Name = "Playerhand1";
+            this.Playerhand1.Size = new System.Drawing.Size(65, 86);
+            this.Playerhand1.TabIndex = 0;
+            this.Playerhand1.TabStop = false;
+            // 
+            // Playerhand2
+            // 
+            this.Playerhand2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Playerhand2.Location = new System.Drawing.Point(182, 533);
+            this.Playerhand2.Name = "Playerhand2";
+            this.Playerhand2.Size = new System.Drawing.Size(65, 86);
+            this.Playerhand2.TabIndex = 1;
+            this.Playerhand2.TabStop = false;
+            // 
+            // Deck
+            // 
+            this.Deck.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Deck.BackgroundImage")));
+            this.Deck.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Deck.Location = new System.Drawing.Point(76, 300);
+            this.Deck.Name = "Deck";
+            this.Deck.Size = new System.Drawing.Size(65, 86);
+            this.Deck.TabIndex = 2;
+            this.Deck.TabStop = false;
+            // 
+            // Playerhand3
+            // 
+            this.Playerhand3.Location = new System.Drawing.Point(217, 533);
+            this.Playerhand3.Name = "Playerhand3";
+            this.Playerhand3.Size = new System.Drawing.Size(65, 86);
+            this.Playerhand3.TabIndex = 3;
+            this.Playerhand3.TabStop = false;
+            // 
+            // Playerhand4
+            // 
+            this.Playerhand4.Location = new System.Drawing.Point(253, 533);
+            this.Playerhand4.Name = "Playerhand4";
+            this.Playerhand4.Size = new System.Drawing.Size(65, 86);
+            this.Playerhand4.TabIndex = 4;
+            this.Playerhand4.TabStop = false;
+            // 
+            // Playerhand5
+            // 
+            this.Playerhand5.Location = new System.Drawing.Point(288, 533);
+            this.Playerhand5.Name = "Playerhand5";
+            this.Playerhand5.Size = new System.Drawing.Size(65, 86);
+            this.Playerhand5.TabIndex = 5;
+            this.Playerhand5.TabStop = false;
+            // 
+            // Playerhand6
+            // 
+            this.Playerhand6.Location = new System.Drawing.Point(324, 533);
+            this.Playerhand6.Name = "Playerhand6";
+            this.Playerhand6.Size = new System.Drawing.Size(65, 86);
+            this.Playerhand6.TabIndex = 6;
+            this.Playerhand6.TabStop = false;
+            // 
+            // Dealbutton
+            // 
+            this.Dealbutton.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Dealbutton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Dealbutton.BackgroundImage")));
+            this.Dealbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Dealbutton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.Dealbutton.Location = new System.Drawing.Point(432, 320);
+            this.Dealbutton.Name = "Dealbutton";
+            this.Dealbutton.Size = new System.Drawing.Size(84, 48);
+            this.Dealbutton.TabIndex = 7;
+            this.Dealbutton.Text = "Deal";
+            this.Dealbutton.UseVisualStyleBackColor = true;
+            // 
+            // Playertotal
+            // 
+            this.Playertotal.AutoSize = true;
+            this.Playertotal.Location = new System.Drawing.Point(214, 639);
+            this.Playertotal.Name = "Playertotal";
+            this.Playertotal.Size = new System.Drawing.Size(0, 13);
+            this.Playertotal.TabIndex = 8;
+            // 
+            // Hitbutton
+            // 
+            this.Hitbutton.Location = new System.Drawing.Point(253, 488);
+            this.Hitbutton.Name = "Hitbutton";
+            this.Hitbutton.Size = new System.Drawing.Size(75, 23);
+            this.Hitbutton.TabIndex = 9;
+            this.Hitbutton.Text = "Hit";
+            this.Hitbutton.UseVisualStyleBackColor = true;
+            // 
+            // Gamewindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(585, 842);
+            this.Controls.Add(this.Hitbutton);
+            this.Controls.Add(this.Playertotal);
+            this.Controls.Add(this.Dealbutton);
+            this.Controls.Add(this.Playerhand6);
+            this.Controls.Add(this.Playerhand5);
+            this.Controls.Add(this.Playerhand4);
+            this.Controls.Add(this.Playerhand3);
+            this.Controls.Add(this.Deck);
+            this.Controls.Add(this.Playerhand2);
+            this.Controls.Add(this.Playerhand1);
+            this.Name = "Gamewindow";
+            this.Text = "Illumicardie Table";
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Deck)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Playerhand6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Playerhand1;
+        private System.Windows.Forms.PictureBox Playerhand2;
+        private System.Windows.Forms.PictureBox Deck;
+        private System.Windows.Forms.PictureBox Playerhand3;
+        private System.Windows.Forms.PictureBox Playerhand4;
+        private System.Windows.Forms.PictureBox Playerhand5;
+        private System.Windows.Forms.PictureBox Playerhand6;
+        private System.Windows.Forms.Button Dealbutton;
+        private System.Windows.Forms.Label Playertotal;
+        private System.Windows.Forms.Button Hitbutton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ illumicardie/illumicardie/Gamewindow.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace illumicardie
+{
+    public partial class Gamewindow : Form
+    {
+        // Tells it what players we will be using
+        private WaveOutEvent backgroundPlayer;
+        private WaveOutEvent soundEffectPlayer;
+        private WaveOutEvent whispersPlayer;
+
+        // Our deck
+        private List<string> deck = new List<string>()
+        {
+            "ace_of_spades", "2_of_spades", "3_of_spades", "4_of_spades", "5_of_spades",
+            "6_of_spades", "7_of_spades", "8_of_spades", "9_of_spades", "10_of_spades",
+            "jack_of_spades", "queen_of_spades", "king_of_spades",
+            "ace_of_hearts", "2_of_hearts", "3_of_hearts", "4_of_hearts", "5_of_hearts",
+            "6_of_hearts", "7_of_hearts", "8_of_hearts", "9_of_hearts", "10_of_hearts",
+            "jack_of_hearts", "queen_of_hearts", "king_of_hearts",
+            "ace_of_diamonds", "2_of_diamonds", "3_of_diamonds", "4_of_diamonds", "5_of_diamonds",
+            "6_of_diamonds", "7_of_diamonds", "8_of_diamonds", "9_of_diamonds", "10_of_diamonds",
+            "jack_of_diamonds", "queen_of_diamonds", "king_of_diamonds",
+            "ace_of_clubs", "2_of_clubs", "3_of_clubs", "4_of_clubs", "5_of_clubs",
+            "6_of_clubs", "7_of_clubs", "8_of_clubs", "9_of_clubs", "10_of_clubs",
+            "jack_of_clubs", "queen_of_clubs", "king_of_clubs"
+        };
+
+        // This is the total listed under the cards
+        private int playerTotal = 0;
+
+        public Gamewindow()
+        {
+            InitializeComponent();
+
+            // Starts music
+            backgroundPlayer = new WaveOutEvent();
+            backgroundPlayer.Init(new AudioFileReader(AppDomain.CurrentDomain.BaseDirectory + @"Sound\Music.mp3"));
+            backgroundPlayer.Volume = 0.1f;
+            backgroundPlayer.Play();
+
+            // Gets the sound effects ready
+            soundEffectPlayer = new WaveOutEvent();
+            whispersPlayer = new WaveOutEvent();
+
+            // Sets Playerhand picture boxes invisible until dealt to
+            Playerhand1.Visible = false;
+            Playerhand2.Visible = false;
+            Playerhand3.Visible = false;
+            Playerhand4.Visible = false;
+            Playerhand5.Visible = false;
+            Playerhand6.Visible = false;
+
+            // Sets the zoom so the cards show right this will need to be done on dealers to
+            Playerhand1.SizeMode = PictureBoxSizeMode.Zoom;
+            Playerhand2.SizeMode = PictureBoxSizeMode.Zoom;
+            Playerhand3.SizeMode = PictureBoxSizeMode.Zoom;
+            Playerhand4.SizeMode = PictureBoxSizeMode.Zoom;
+            Playerhand5.SizeMode = PictureBoxSizeMode.Zoom;
+            Playerhand6.SizeMode = PictureBoxSizeMode.Zoom;
+
+            // makes it so they cant HIT until dealt to
+            Hitbutton.Enabled = false;
+
+            // linsk the method to the clicky
+            Dealbutton.Click += Dealbutton_Click;
+
+            // links the method to the clicky
+            Hitbutton.Click += Hitbutton_Click;
+        }
+
+        // Method to load the cardsr
+        private void LoadCardImage(PictureBox pictureBox, string card)
+        {
+            // Construct the file path for the card image
+            string imagePath = @"Images\" + card + ".png";
+
+            // Load the card image into the specified picture box
+            pictureBox.Image = Image.FromFile(imagePath);
+
+            // Make the picture box visible
+            pictureBox.Visible = true;
+        }
+
+        // Method to deal the cards
+        private string DealCard()
+        {
+            // Check if the deck is empty
+            if (deck.Count == 0)
+            {
+                MessageBox.Show("The deck is empty!");
+                return null;
+            }
+
+            // Generate a random index to select a card from the deck
+            Random random = new Random();
+            int index = random.Next(deck.Count);
+
+            // Get the card at the selected index
+            string card = deck[index];
+
+            // Remove the dealt card from the deck
+            deck.RemoveAt(index);
+
+            return card;
+        }
+
+        // Method to update the player's total score
+        private void UpdatePlayerTotal(string card)
+        {
+            // Extract the card value
+            int value;
+            if (int.TryParse(card.Split('_')[0], out value))
+            {
+                // Card is a number card (2, 3, ..., 10)
+                playerTotal += value;
+            }
+            else
+            {
+                // Card is a face card or ace
+                if (card.StartsWith("ace"))
+                {
+                    // Ace is worth 11
+                    playerTotal += 11;
+                }
+                else
+                {
+                    // Face cards are worth 10
+                    playerTotal += 10;
+                }
+            }
+
+            // Update the Playertotal label
+            Playertotal.Text = "Player Total: " + playerTotal.ToString();
+        }
+
+        //Dealbutton click event
+        private void Dealbutton_Click(object sender, EventArgs e)
+        {
+            // Stop sounds
+            soundEffectPlayer.Stop();
+            whispersPlayer.Stop();
+
+            // Play the sounds
+            soundEffectPlayer.Init(new AudioFileReader(AppDomain.CurrentDomain.BaseDirectory + @"Sound\Gong.mp3"));
+            soundEffectPlayer.Play();
+
+            whispersPlayer.Init(new AudioFileReader(AppDomain.CurrentDomain.BaseDirectory + @"Sound\Whispers.mp3"));
+            whispersPlayer.Play();
+
+            // Deal a card to Playerhand1
+            string card1 = DealCard();
+            if (card1 != null)
+            {
+                LoadCardImage(Playerhand1, card1);
+                UpdatePlayerTotal(card1);
+            }
+
+            // Deal a card to Playerhand2
+            string card2 = DealCard();
+            if (card2 != null)
+            {
+                LoadCardImage(Playerhand2, card2);
+                UpdatePlayerTotal(card2);
+            }
+
+            // Disable the Deal button after the deal
+            Dealbutton.Enabled = false;
+
+            // Enable the Hit button after the deal
+            Hitbutton.Enabled = true;
+        }
+
+        // Hitbutton click 
+        private void Hitbutton_Click(object sender, EventArgs e)
+        {
+            // Find the first empty picture box from Playerhand3 to Playerhand6
+            PictureBox emptyPictureBox = null;
+            if (Playerhand3.Image == null)
+            {
+                emptyPictureBox = Playerhand3;
+            }
+            else if (Playerhand4.Image == null)
+            {
+                emptyPictureBox = Playerhand4;
+            }
+            else if (Playerhand5.Image == null)
+            {
+                emptyPictureBox = Playerhand5;
+            }
+            else if (Playerhand6.Image == null)
+            {
+                emptyPictureBox = Playerhand6;
+            }
+
+            // deal a card to it
+            if (emptyPictureBox != null)
+            {
+                string card = DealCard();
+                if (card != null)
+                {
+                    LoadCardImage(emptyPictureBox, card);
+
+                    // picture box to the front
+                    emptyPictureBox.BringToFront();
+
+                    UpdatePlayerTotal(card);
+
+                    //total exceeds 21 (BUST)
+                    if (playerTotal > 21)
+                    {
+                        PlayCrowdLoseSound();
+                        PlayGunshotSound();
+                        PlayScreamSound();
+                        whispersPlayer.Stop();
+                        // BUST
+                        MessageBox.Show("BUST! Your total exceeded 21.", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        // Reset 
+                        ResetGame();
+                    }
+                }
+            }
+        }
+
+        // Method to reset the game
+        private void ResetGame()
+        {
+            // Clear all picture boxes and set them back to invisible
+            Playerhand1.Image = null;
+            Playerhand1.Visible = false;
+            Playerhand2.Image = null;
+            Playerhand2.Visible = false;
+            Playerhand3.Image = null;
+            Playerhand3.Visible = false;
+            Playerhand4.Image = null;
+            Playerhand4.Visible = false;
+            Playerhand5.Image = null;
+            Playerhand5.Visible = false;
+            Playerhand6.Image = null;
+            Playerhand6.Visible = false;
+
+            // Reset player total
+            playerTotal = 0;
+
+            // Update Playertotal label
+            Playertotal.Text = "Player Total: " + playerTotal.ToString();
+
+            // Enable the Deal button
+            Dealbutton.Enabled = true;
+
+            // Disable the Hit button
+            Hitbutton.Enabled = false;
+            
+
+        }
+        private void PlayCrowdLoseSound()
+        {
+            WaveOutEvent crowdLosePlayer = new WaveOutEvent();
+            crowdLosePlayer.Init(new AudioFileReader(AppDomain.CurrentDomain.BaseDirectory + @"Sound\Crowdlose.mp3"));
+            crowdLosePlayer.Play();
+        }
+
+        // Method to play Gunshot sound effect
+        private void PlayGunshotSound()
+        {
+            WaveOutEvent gunshotPlayer = new WaveOutEvent();
+            gunshotPlayer.Init(new AudioFileReader(AppDomain.CurrentDomain.BaseDirectory + @"Sound\Gunshot.mp3"));
+            gunshotPlayer.Play();
+        }
+        private void PlayScreamSound()
+        {
+            WaveOutEvent screamPlayer = new WaveOutEvent();
+            screamPlayer.Init(new AudioFileReader(AppDomain.CurrentDomain.BaseDirectory + @"Sound\Scream.mp3"));
+            screamPlayer.Play();
+        }
+    }
+}
